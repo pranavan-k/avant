a880d83eac7baca755085c39dd351322a5c65920
Pranavan <pranavan.krishna23@gmail.com>
2023-08-13 12:15:15 -0400

    minor changes to the home page and added meta data

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
 import Image from "next/image";
+import Head from "next/head"
+import type { Metadata } from 'next'
+import home_splash from "../public/images/home-splash2.png"
+
+export const metadata: Metadata = {
+  description: '...',
+}
 
 export default function Home() {
   /*
@@ -25,7 +32,18 @@ export default function Home() {
   */
   return (
     <div>
-      <h1>Hello</h1>
+      <main>
+        <div className="flex lg:flex-row flex-col w-screen h-screen">
+          <div className="lg:w-1/2 lg:p-32 w-screen">
+            <Image className="lg:w-full h-auto" src={home_splash} alt="WaveCO homepage image" sizes="100vw"/>
+          </div>
+          <div className="lg:w-1/2 w-screen flex items-center text-center flex-col">
+            <h2>Shop The Latest Streetwear</h2>
+            <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quos deserunt beatae dolor eligendi provident, earum incidunt praesentium. Omnis temporibus fugit esse natus quas ex sed illo, nisi incidunt error.</p>
+          </div>
+        </div>
+      </main>
     </div>
+    
   );
 }
--- app/products/tees/page.tsx
+++ app/products/tees/page.tsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: "Tees",
+  description: '...',
+}
 
 function Tees() {
   return (
