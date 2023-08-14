6d17c53096e067fe4316b3f365a526edb88e66cc
Pranavan <pranavan.krishna23@gmail.com>
2023-08-14 13:24:45 -0400

    fixes

--- app/products/tees/page.tsx
+++ app/products/tees/page.tsx
@@ -1,37 +1,16 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import type { Metadata } from "next";
-import Product from "@/app/components/product";
+import TeesDisplay from "@/app/components/teesDisplay";
 
 export const metadata : Metadata = {
   title: "Tees"
 }
 
 function Tees() {
-  const [productsData, setFetchedData] = useState([]);
-
-  const fetchedData = async () => {
-    const res = fetch(`/api/products/tees`, {
-      method: "GET",
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setFetchedData(data);
-      });
-  };
-
-  useEffect(() => {
-    fetchedData();
-  }, []);
-
   return (
-    <main className="w-screen flex justify-around lg:flex-row flex-col">
-      {productsData.map((product: any, key) => {
-        return (<Product key={key} id={product._id} name={product.name} imageId={product.imageId} price={product.price} qty={product.qty}/>);
-      })}
-    </main>
+    <div>
+      <TeesDisplay />
+    </div>
   );
 }
 
