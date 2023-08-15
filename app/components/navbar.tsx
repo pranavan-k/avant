fe3738e1b3e6039555b8e5f5d33737b0e63a7ef2
Pranavan <pranavan.krishna23@gmail.com>
2023-08-15 18:07:17 -0400

    changes the entire comapny lol

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import HamburgerMenu from "./hamburgerMenu";
 import BagMenu from "./bagMenu";
 import { Tajawal } from "next/font/google";
+import Logo from "@/public/images/avant-text.png"
 
 const ysabeau_infant = Tajawal({
   weight: ["400"],
@@ -12,15 +13,14 @@ const ysabeau_infant = Tajawal({
 
 function Navbar() {
   return (
-    <nav className="w-screen flex items-center bg-white fixed top-0 z-50 py-2">
+    <nav className="w-screen flex items-center bg-white fixed top-0 z-50">
       <div className="flex w-1/2 lg:w-1/3 justify-center items-center">
         <Link href="/">
           <Image
-            className="h-auto w-auto"
-            src="/waveco_logo.png"
+            className="w-24"
+            src={Logo}
             alt="waveco logo"
-            width={120}
-            height={50}
+            sizes="100%"
           />
         </Link>
       </div>
--- app/products/tees/page.tsx
+++ app/products/tees/page.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import type { Metadata } from "next";
 import TeesDisplay from "@/app/components/teesDisplay";
+import Image from "next/image";
+import TeesSplash from "@/public/images/tees-splash-image.png"
 
 export const metadata : Metadata = {
-  title: "Shop Tees - Streetwear and graphic tees"
+  title: "Shop Tees: Streetwear and graphic tees by WaveCO"
 }
 
 function Tees() {
   return (
-    <div>
+    <main>
+      <div className="flex justify-center">
+        <Image src={TeesSplash} alt="tees page image - streetwear model" sizes="90vw" className="lg:w-2/5"/>
+        <div className="flex flex-col justify-center">
+          <h1>Shop Graphic Tees</h1>
+          <p>Streetwear and graphic tees that suit every occasion</p>
+        </div>
+      </div>
       <TeesDisplay />
-    </div>
+    </main>
   );
 }
 
